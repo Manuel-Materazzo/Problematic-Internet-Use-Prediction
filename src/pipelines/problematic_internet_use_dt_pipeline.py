f8eacaa6e283ecd620ad4d83b42ec8f239e9c096
Manuel <manuelmaterazzo@gmail.com>
2024-12-11 21:59:46 +0100

    pipeline and data description update

---
+++ src/pipelines/problematic_internet_use_dt_pipeline.py
@@ -0,0 +1,25 @@
+from pandas import DataFrame
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from src.pipelines.dt_pipeline import DTPipeline
+
+
+class ProblematicInternetUseDTPipeline(DTPipeline):
+    def __init__(self, X: DataFrame, imputation_enabled: bool):
+        super().__init__(X, imputation_enabled)
+
+    def build_pipeline(self) -> Pipeline | ColumnTransformer:
+
+        # Preprocessing for numerical data
+        numerical_transformer = SimpleImputer(strategy='median')
+
+        preprocessor = ColumnTransformer(transformers=[
+                ('num', numerical_transformer, self.numerical_cols)
+        ])
+
+        # Bundle preprocessing
+        return Pipeline(steps=[
+            ('preprocessor', preprocessor)
+        ], memory=None)
