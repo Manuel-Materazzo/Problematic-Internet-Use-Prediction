c7bb56e52e237577a32896fd445bd17a461eee47
Manuel <manuelmaterazzo@gmail.com>
2025-01-16 01:03:16 +0000

    Pull changes from BoostUtils

--- tests/data_load.py
+++ tests/data_load.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def load_regression_data():
     # Load the data
-    file_path = os.path.dirname(os.path.realpath(__file__)) + '../../resources/regression_unit_test.csv'
+    file_path = os.path.dirname(os.path.realpath(__file__)) + '/../resources/regression_unit_test.csv'
     data = pd.read_csv(file_path)
     # standardize column names
     data = data.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '_', x))
@@ -19,7 +19,7 @@ def load_regression_data():
 
 def load_classification_data():
     # Load the data
-    file_path = os.path.dirname(os.path.realpath(__file__)) + '../../resources/classification_unit_test.csv'
+    file_path = os.path.dirname(os.path.realpath(__file__)) + '/../resources/classification_unit_test.csv'
     data = pd.read_csv(file_path)
     # standardize column names
     data = data.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '_', x))
